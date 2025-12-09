df71a8b1bfd643d004f20e8d00a20261cabe6476
GitHub Action <action@github.com>
2025-12-09 08:42:10 +0000

    🚀 Auto-update cache version to v20251209-084210-9754948

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@
         - Badges: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=application-badges
     */
 
-    const CACHE_VERSION = 'v20250813-153818-ec7fac0';
+    const CACHE_VERSION = 'v20251209-084210-9754948';
     const CACHE_NAME = `tennis-tournament-finder-${CACHE_VERSION}`;
 
     const HOSTNAME_WHITELIST = [
